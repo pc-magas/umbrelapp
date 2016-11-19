6582a217885ad98172f32e38702d06534a9871c5
Dimitrios Desyllas (pc_magas) <you@example.com>
2016-11-19 17:44:28 +0200

    Configure rthe connection

--- application/config/db.js
+++ application/config/db.js
@@ -1,13 +1,43 @@
 //Setting Up Database
-var knex = require('knex')({
-  client: 'pg',
-  connection: {
-    host : process.env.UMBRELAPP_DB_HOST,
-    user : process.env.UMBRELAPP_DB_USER,
-    password : process.env.UMBRELAPP_DB_PASSWD,
-    database : process.env.UMBRELAPP_DB_NAME
-  }
-});
 
+/**
+* Create the connection String
+*
+* @param string username The database user username
+* @param string password The database user password
+* @param string database The database name
+* @param string host Where the database is hosted if empty 'localhost' assumed
+* @param string | int port The port for the connection
+*
+* @return string
+*/
+var connection=function(username,password,database,host,port)
+{
+  if(!port)
+  {
+      port=5432
+  }
+
+  if(!host)
+  {
+    host='localhost'
+  }
+
+  return "pg://"+username+':'+password+'@'+host+':'+port+'/'+database
+}
+
+
+var connection_info={
+  client: 'pg',
+  connection: connection(
+                         process.env.UMBRELAPP_DB_USER,
+                         process.env.UMBRELAPP_DB_PASSWD,
+                         process.env.UMBRELAPP_DB_NAME,
+                         process.env.UMBRELAPP_DB_HOST,
+                         process.env.UMBRELAPP_DB_PORT
+                       )
+};
+
+var knex = require('knex')(connection_info);
 
 module.exports=knex;
