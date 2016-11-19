533bb39d0e4297bf14c2ed44b8cb32453580053e
Dimitrios Desyllas (pc_magas) <you@example.com>
2016-11-19 19:06:44 +0200

    Select weather

---
+++ application/libs/model_status.js
@@ -0,0 +1,86 @@
+function ActionStatus()
+{
+  var self=this;
+
+  /**
+  * Indicates if a status id successfull or not
+  */
+  var status=null;
+
+  /**
+  * Usefull Constants in order to define the ok and errorous status
+  */
+  const OK=true;
+  const ERROR=false;
+
+  /**
+  * Method that indicates that the action youy didi has a successfull result
+  */
+  self.statusOK=function()
+  {
+    status=OK;
+  }
+
+  /**
+  * Returns true if this status action is ok
+  * @return boolean
+  */
+  self.isOk=function()
+  {
+    return status===OK;
+  }
+
+  /**
+  * Error type Literals
+  */
+  self.errorTypes={
+    /**
+    * Error that indicates that a parameter is misssing
+    */
+    'missing_param':1,
+    /**
+    * Error that Indicates that a parameter has not giver correctly
+    */
+    'wrong_param':2,
+    /**
+    * Error that Indicates an Internal Error
+    */
+    'internal':3
+  }
+
+  self.error_type=null
+  /**
+  * Set this status action as
+  * @param integer type
+  */
+  self.statusError=function(type)
+  {
+    if(
+        type===self.errorTypes.missing_param ||
+        type===self.errorTypes.wrong_param  ||
+        type===self.errorTypes.internal
+      ){
+          status=ERROR;
+          self.error_type=type;
+
+      }
+      else {
+          throw Error('This error type is not supported');
+      }
+  }
+
+  /**
+  * Set the message of the following status
+  * @var string
+  */
+  self.message=null
+
+  /**
+  * Set the data of the action
+  * @var mixed
+  */
+  self.data=null
+}
+
+
+module.exports=ActionStatus;
---
+++ application/models/weather.js
@@ -0,0 +1,104 @@
+var db=require('../config/db.js');
+var ActionStatus=require('../libs/model_status.js');
+var moment=require('moment');
+
+function Weather()
+{
+  var self=this;
+
+  var table='weather';
+
+  /**
+  * Method we use to search the weather from the database
+  *
+  * @param timestamp string from_time The date we want the weather forecast
+  * @param unsigned int forecast_duration How many days we want the forecast
+  * @param city_id the id of the city we want the forecast
+  * @param city_name The name of the city we want the forecast
+  * @param function callback A callback that is used to return the data
+  *
+  */
+  self.search=function(params,callback)
+  {
+    var returnStatus = new ActionStatus();
+
+    query=db.select([table+'.id',table+'.date',table+'.temperature_max',table+'.temperature_min','city.name','city.country'])
+            .withSchema('public')
+            .from(table)
+            .join('city',table+'.city_id','city.id')
+
+    if(params.from_time)
+    {
+      //Validating date
+      var date=moment(params.from_time,['YYYY-MM-DD HH:mm:ss','YYYY-MM-DD']);
+      if(!date.isValid())
+      {
+        returnStatus.statusError(returnStatus.errorTypes.wrong_param);
+        returnStatus.message="The startup datetime you given is not the correct one";
+        callback(returnStatus);
+
+        return;
+      }
+
+      //Selecting Duration
+      if(params.duration)
+      {
+        console.log(parseInt(params.duration));
+        var date_end=moment(date.toDate()).add(parseInt(params.duration),'days');
+
+        console.log(date.format('YYYY-MM-DD'),date_end)
+
+        query.whereBetween(table+'.date',[date.format('YYYY-MM-DD'),date_end.format('YYYY-MM-DD')])
+      }
+
+      //Selecting by date
+      query.where(table+'.date',date.format('YYYY-MM-DD'));
+
+    }
+    else if(params.duration) //Duration given only
+    {
+
+      returnStatus.statusError(returnStatus.wrong_param);
+      returnStatus.message="For duration you must specify the start date.";
+
+      callback(returnStatus);
+    }
+
+    // Searching by City
+    if(params.city_id)
+    {
+      if(params.city_name)
+      {
+        returnStatus.statusError(returnStatus.wrong_param);
+        returnStatus.message="Only City id must be given";
+        callback(returnStatus);
+      }
+
+      query.where(table+'.city_id',params.city_id);
+    }
+    else if(params.city_name)
+    {
+      params.city_name=params.city_name.split(',');
+
+      query.where('city.name',params.city_name[0].trim());
+
+      // If country spefified
+      if(params.city_name[1])
+      {
+        query.where('city.country',params.city_name[1].trim());
+      }
+    }
+
+    // Returning the data
+    query.then(function(data)
+    {
+        returnStatus.isOk();
+        returnStatus.data=data;
+        callback(returnStatus);
+    });
+
+  }
+
+}
+
+module.exports=Weather;
